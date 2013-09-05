e5c07507759fce82515dc4026474e628e98cea6a
Arne König <arne.ko@23inch.de>
2013-09-05 14:44:27 +0000

    forcing browser to reload new versions

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 build="build"
 name="${build}/underthecrypt"
-files="src/spirit.js src/level.js src/spawner.js src/tower.js src/ui.js src/game.js src/journal.js";
+files="src/spirit.js src/level.js src/spawner.js src/tower.js src/ui.js src/game.js src/journal.js"
+time=`date +%s`
 if [ -d $name ]; then
     rm -f $name/*
 else
@@ -15,6 +16,9 @@ cp src/tiles.png src/ghost.png $name/
 cp src/index.c.html $name/index.html
 cp src/journal.c.html $name/journal.html
 cp src/under.c.manifest $name/under.manifest
+echo -e "\n$time" >> $name/under.manifest
+touch $name/$time
+
 echo "compiling"
 #java -jar compiler.jar --warning_level VERBOSE --compilation_level ADVANCED_OPTIMIZATIONS --externs src/extern.js --js ${files} --js_output_file $name/utc.js
 java -jar compiler.jar --jscomp_warning checkTypes --compilation_level ADVANCED_OPTIMIZATIONS --use_types_for_optimization --externs src/extern.js --js ${files} --js_output_file $name/utc.js
